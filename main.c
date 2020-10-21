6349a0f9367190cce4122bb64ae875671059edfd
ellyzinha <adriellylovato@gmail.com>
2020-10-20 23:32:55 -0300

    adicionando personagens e assets

--- main.c
+++ main.c
@@ -28,14 +28,45 @@
         int altura;
         int largura;
         float scrollingBack;
+        int vida;
     }MISTY;
+    
+    typedef struct {
+        Vector2 pos;
+        Vector2 vel;
+        Vector2 acc;
+        Texture2D texture_enemy;
+        Rectangle sourceRec_enemy;
+        Texture2D textura_background;
+        int frameAtual;
+        int altura;
+        int largura;
+        float scrollingBack;
+    }ENEMY;
+    
+    void initEnemy(ENEMY* enemy) {
+        enemy->pos.x = 1000;
+        enemy->pos.y = 650;
+        enemy->vel.y = 0;
+        enemy->vel.x = 0;
+        enemy->acc.y = 0.7;
+        enemy->acc.x = 0;
+        enemy->altura;
+        enemy->largura;
+        enemy->texture_enemy = LoadTexture("./assets/personagens/demon-idle.png");
+        Rectangle sr_enemy = {0.0f, 0.0f, enemy->texture_enemy.width/6, enemy->texture_enemy.height};
+        enemy->sourceRec_enemy = sr_enemy;
+       // enemy->textura_background = LoadTexture("./background/level1/forest.png");
+       // enemy->scrollingBack = 0.0f;
+        enemy->frameAtual = 0;
+    }
     
     void initMisty(MISTY* misty){
         misty->pos.x = 0;
         misty->pos.y = 650;
         misty->vel.y = 0;
         misty->vel.x = 0;
-        misty->acc.y = 0.2;
+        misty->acc.y = 0.5;
         misty->acc.x = 0;
         misty->altura;
         misty->largura;
@@ -54,9 +85,40 @@
         misty->textura_attack = LoadTexture("./assets/WitchSprite/attack.png");
         Rectangle sr_attack = {0.0f, 0.0f, misty->textura_attack.width/3, misty->textura_attack.height};
         misty->sourceRec_attack = sr_attack;
-       // misty->textura_background = LoadTexture("./background/level1/forest.png");
-       // misty->scrollingBack = 0.0f;
+        misty->textura_background = LoadTexture("./background/level1/forest.png");
+        misty->scrollingBack = 0.0f;
         misty->frameAtual = 0;
+        misty->vida = 3;
+    }
+    
+    void updateEnemy(ENEMY* enemy) {
+        static int frameCounter = 0;
+        frameCounter++;
+        
+        if(frameCounter > 10) {
+            frameCounter = 0;
+            enemy->frameAtual++;
+            
+            if(enemy->frameAtual > 5) {
+                enemy->frameAtual = 0;
+            }
+        }
+        
+        enemy->vel.x += enemy->acc.x;
+        enemy->vel.y += enemy->acc.y;
+        
+        enemy->pos.x += enemy->vel.x;
+        enemy->pos.y += enemy->vel.y;
+    
+        if(enemy->pos.x < 1000) enemy->pos.x = 1000;
+        if(enemy->pos.x > 1150) enemy->pos.x = 1150;
+        if(enemy->pos.y < 0) enemy->pos.y = 0;
+        if(enemy->pos.y + enemy->texture_enemy.height > screenHeight) enemy->pos.y = screenHeight - enemy->texture_enemy.height;
+    
+    
+        enemy->sourceRec_enemy.x = enemy->frameAtual*enemy->texture_enemy.width/6;
+        
+        
     }
     
     void updateMisty(MISTY* misty) {
@@ -108,6 +170,16 @@
     
     
         misty->sourceRec_walk_right.x = misty->frameAtual*misty->textura_walk_right.width/4;
+        
+        //Colisão
+        
+    }
+    
+    void drawEnemy(ENEMY* enemy) {
+        //DrawTextureEx(enemy->textura_background, (Vector2){enemy->scrollingBack, 20 }, 0.0f, 2.0f, WHITE);
+        //DrawTextureEx(enemy->textura_background, (Vector2){enemy->textura_background.width*2 + enemy->scrollingBack, 20 }, 0.0f, 2.0f, WHITE);
+        
+        DrawTextureRec(enemy->texture_enemy, enemy->sourceRec_enemy, enemy->pos, WHITE);
     }
     
     void drawMisty(MISTY* misty) {
@@ -140,6 +212,9 @@ int main(void) {
     MISTY* misty;
     initMisty(&misty);
     
+    ENEMY* enemy;
+    initEnemy(&enemy);
+    
     
     while (!WindowShouldClose()) {
         
@@ -148,6 +223,8 @@ int main(void) {
         
         updateMisty(&misty);
         drawMisty(&misty);
+        updateEnemy(&enemy);
+        drawEnemy(&enemy);
         
         EndDrawing();
         
